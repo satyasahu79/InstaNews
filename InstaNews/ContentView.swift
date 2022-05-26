729212bbe33f69e9360125004973a79b13da5a8a
Satya Prakash Sahu <satyasahu79@gmail.com>
2022-05-26 16:54:11 +0530

    Initial Commit

---
+++ InstaNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaNews
+//
+//  Created by Satya Prakash Sahu on 26/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaNews/InstaNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaNewsApp.swift
+//  InstaNews
+//
+//  Created by Satya Prakash Sahu on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
